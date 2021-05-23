a1438145aa11384c2b264401ac028022d1711aaa
karoolina1234 <karoolina.sm@gmail.com>
2021-05-23 02:29:26 -0300

    pagina de cadastro

--- src/pages/SiginUp/index.js
+++ src/pages/SiginUp/index.js
@@ -3,19 +3,24 @@ import { View, Text } from 'react-native';
 import {
   Background, Container,
   Logo, AreaInput, Input, SubmitButton, SubmitText, Link, LinkText
-} from './styles'
-import { useNavigation } from "@react-navigation/native"
+} from '../SignIn/styles'
 function SignIn() {
-
-  const navigation = useNavigation();
-
+  const [nome, setNome] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   return (
     <Background>
       <Container>
-        <Logo source={require('../../assets/img/Logo.png')} />
+        <AreaInput>
+          <Input
+            placeholder="Nome"
+            autoCorrect={false}
+            autoCapitalize="none"
+            value={nome}
+            onChangeText = {(text) => setNome(text)}
+          />
+        </AreaInput>
         <AreaInput>
           <Input
             placeholder="E-mail"
@@ -36,12 +41,9 @@ function SignIn() {
         </AreaInput>
         <SubmitButton>
           <SubmitText>
-            Acessar
+            Cadastrar
           </SubmitText>
         </SubmitButton>
-        <Link onPress={()=>navigation.navigate('SignUp')}>
-          <LinkText>Criar uma conta</LinkText>
-        </Link>
       </Container>
     </Background>
   )
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import {createStackNavigator} from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 //Pages
 import SignIn from '../pages/SignIn'
+import SignUp from '../pages/SiginUp'
 
 const AuthStack = createStackNavigator()
 
-function AuthRoutes (){
-    return(
+function AuthRoutes() {
+    return (
         <AuthStack.Navigator>
-            <AuthStack.Screen  
-            name="SignIn" 
-            component={SignIn}
-            options={{headerShown: false}}/>
+            <AuthStack.Screen
+                name="SignIn"
+                component={SignIn}
+                options={{ headerShown: false }} />
+            <AuthStack.Screen
+                name="SignUp"
+                component={SignUp}
+                options={{
+                    headerStyle:{
+                        backgroundColor:"#131313",
+                        borderBottomWidth:1,
+                        borderBottomColor:"#00b94a"
+                    },
+                    headerTintColor:"#FFF",
+                    headerBackTitleVisible: false,
+                    headerTitle:"Voltar"
+                }}
+            />
         </AuthStack.Navigator>
     )
 }
